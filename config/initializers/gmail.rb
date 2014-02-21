fe13bba47625b835445120ca4ad35e0ec25e2505
TortronUA <nazariy.papizh@gmail.com>
2014-02-21 13:40:22 +0200

    test 3

--- config/initializers/gmail.rb
+++ config/initializers/gmail.rb
@@ -3,10 +3,10 @@ ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
     #:domain               => "voroninstudio.eu",
-    #:user_name            => "support",
-    #:password             => "Studiosupport123",
-    :user_name           => "partido12",
-    :password             => "pasha1991",
+    :user_name            => "support",
+    :password             => "Studiosupport123",
+    #:user_name           => "partido12",
+    #:password             => "pasha1991",
     :authentication       => "plain",
     :enable_starttls_auto => true
 }

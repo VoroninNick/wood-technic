c3ec7397f138bbbd419472b55523b3ff6c577c7f
TortronUA <nazariy.papizh@gmail.com>
2014-02-21 12:56:56 +0200

    empty

--- config/initializers/gmail.rb
+++ config/initializers/gmail.rb
@@ -1,8 +1,7 @@
 # -*- encoding : utf-8 -*-
 ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",
-    #:port                 => 587,
-    :port                 => 465,
+    :port                 => 587,
     :domain               => "voroninstudio.eu",
     :user_name            => "support",
     :password             => "Studiosupport123",
